860cea9f5fd483eac3c4e67a71f648778ffd6a53
manoj-kumar143 <manojkumarpolimera47@gmail.com>
2025-03-28 15:18:34 +0530

    Create char.py

---
+++ week 2/char.py
@@ -0,0 +1,10 @@
+n = input("enter the string: ")[0]
+if (n.isupper()):
+    print("n is uppercase")
+elif (n.islower()):
+    print("n is lowercase")
+elif (n.isdigit()):
+    print("n is a number")
+else:
+    print("n is a special character")
+ 
