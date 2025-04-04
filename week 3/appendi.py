2ab538ca8e076bc409a0dfb839715dc2b6a6b9d3
manoj-kumar143 <manojkumarpolimera47@gmail.com>
2025-04-04 15:53:17 +0530

    Create appendi.py

---
+++ week 3/appendi.py
@@ -0,0 +1,8 @@
+elements = []
+limit = int(input("enter the length: "))
+for i in range(limit):
+    temp=int(input("Enter the list: "))
+    elements.append(temp)
+for i in elements:
+    print(i,end=' ')
+    
