5e9d6e26c2c2501300cbc596ccca7cd94e7353e9
manoj-kumar143 <manojkumarpolimera47@gmail.com>
2025-04-04 16:10:08 +0530

    Create appendfun.py

---
+++ week 3/appendfun.py
@@ -0,0 +1,8 @@
+def print_list(elements):
+    print(elements)
+elements = []
+limit = int(input("enter the length: "))
+for i in range(limit):
+    temp=int(input("Enter the list: "))
+    elements.append(temp)
+print_list(elements)
