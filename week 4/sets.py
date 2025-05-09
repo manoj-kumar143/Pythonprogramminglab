2a4983fc95edbe3391335147e28d1db4b2fd4db1
manoj-kumar143 <manojkumarpolimera47@gmail.com>
2025-05-09 10:25:35 +0530

    Create sets.py

---
+++ week 4/sets.py
@@ -0,0 +1,32 @@
+# thisset = {"apple", "banana", "cherry"}
+# print(thisset)
+
+
+# thisset = {"apple", "banana", "cherry", "apple"}
+
+# print(thisset)
+
+
+# thisset = {"apple", "banana", "cherry", True, 1, 2}
+
+# print(thisset)
+
+
+# thisset = {"apple", "banana", "cherry", False, True, 0}
+
+# print(thisset)
+
+
+# thisset = {"apple", "banana", "cherry"}
+
+# print(len(thisset))
+
+
+# myset = {"apple", "banana", "cherry"}
+# print(type(myset))
+
+
+# thisset = set(("apple", "banana", "cherry")) # note the double round-brackets
+# print(thisset)
+
+
